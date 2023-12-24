610edbf75e275c4acf63b002d85f93147fc6fec1
Yuri <laquicuong.rou@gmail.com>
2023-12-24 21:21:10 +0700

    Change code Css and style design web

--- page/user/index.php
+++ page/user/index.php
@@ -7,19 +7,36 @@
 <!--Tạo một hiển thị-->
 <div class="app-item">
     <!--Phần thông tin một hiển thị-->
-    <center>
-        <div class="app-item-info">
-            <div class="i-caption FuncAlert">
-                <p>Tiếp tục với tài khoản Iwanto</p>
+    <div class="app-item-info">
+    <div class="app-item-media">
+       
+    </div>
+        <center>
+            <div class="i-caption FuncBtn">
+            <p CallText="qwe">Tiếp tục với Iwanto</p>
             </div>
-        </div>
-    </center>
+        </center>
+    </div>
     <!--Phần hiển thị một phương tiện-->
     <!--Các mở rộng tùy chọn-->
     <div class="app-item-open">
-        <div class="choose"><i class="fas fa-sign-in-alt"></i> <span title="Đăng nhập"></span></div>
-        <div class="choose"><i class="fas fa-user-plus"></i> <span title="Đăng kí"></span></div>
-        <div class="choose"><i class="fas fa-lock"></i> <span title="Quên?"></span></div>
+        <div class="choose" DataCheck="login"><i class="fas fa-sign-in-alt"></i> <span title="Đăng nhập"></span></div>
+        <div class="choose" DataCheck="signup"><i class="fas fa-user-plus"></i> <span title="Đăng kí"></span></div>
+        <div class="choose" DataCheck="forget"><i class="fas fa-lock"></i> <span title="Quên?"></span></div>
     </div>
 </div>
 <!--Kết thúc một hiển thị-->
+<style>
+
+</style>
+<script>
+    $(".choose[DataCheck='login']").click(()=>{
+        $("[CallText='qwe']").html("Đăng nhập");
+    });
+    $(".choose[DataCheck='signup']").click(()=>{
+        $("[CallText='qwe']").html("Đăng kí");
+    });
+    $(".choose[DataCheck='forget']").click(()=>{
+        $("[CallText='qwe']").html("Forget pass");
+    });
+</script>
